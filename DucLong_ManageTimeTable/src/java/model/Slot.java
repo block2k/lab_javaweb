cc3ba446fde4e11f1e79955ebe129293ffaba2ab
block2k <longbdhe140529@fpt.edu.vn>
2021-08-12 12:05:01 +0700

    Bài Manage Timetable

---
+++ DucLong_ManageTimeTable/src/java/model/Slot.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Slot {
+
+    private int id;
+    private String slotName;
+
+    public Slot() {
+    }
+
+    public Slot(int id, String slotName) {
+        this.id = id;
+        this.slotName = slotName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSlotName() {
+        return slotName;
+    }
+
+    public void setSlotName(String slotName) {
+        this.slotName = slotName;
+    }
+
+}
